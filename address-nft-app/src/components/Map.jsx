87888eccc5c75adc9edd84f32f725790b3c5bf63
Vedantjn <vedant.jain.indore@gmail.com>
2024-07-22 18:11:52 +0530

    first commit

--- address-nft-app/src/components/Map.jsx
+++ address-nft-app/src/components/Map.jsx
@@ -51,11 +51,7 @@ const Map = ({ selected }) => {
   }, [selected]);
 
   return (
-    <MapWrapper
-      initial={{ opacity: 0, scale: 0.8 }}
-      animate={{ opacity: 1, scale: 1 }}
-      transition={{ duration: 0.5 }}
-    >
+    <MapWrapper>
       <div ref={mapContainerRef} style={{ width: '100%', height: '100%' }} />
     </MapWrapper>
   );
